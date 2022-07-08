a5e51569fb8578ab6ad548b8c6aa6891ae5bbd9a
Michael Garber <michael.garber@hedera.com>
2022-07-08 12:32:52 -0400

    add 3 more tests
    Signed-off-by: Michael Garber <michael.garber@hedera.com>

--- test/web3js.spec.js
+++ test/web3js.spec.js
@@ -13,10 +13,23 @@ dotenv.config();
 
 
 describe("Web3js", function () {
-    it("should get the account object when valid eth private key is provided", async function() {
+    it("should reject if contract is called without setting an account", function() {
+        expect(() => web3.eth.getBalance()).throws(/Provided address undefined is invalid/);
+    });
+    it("should get the account object when valid eth private key is provided", function () {
         const account = web3.eth.accounts.privateKeyToAccount(process.env.ETH_PRIVATE_KEY);
         expect(Object.keys(account).length).to.equal(5);
         expect(account.address).to.equal('0xC2A3C271e2D133ea12ea2BC747fA0780da134136');
         expect(account.privateKey).to.equal(process.env.ETH_PRIVATE_KEY);
-    })
+    });
+    it("should get the contract object if valid contract address is provided", function () {
+        const contract = new web3.eth.Contract(MyContract.abi, process.env.CONTRACT_ADDRESS);
+        expect(Object.keys(contract.methods).length).to.equal(6);
+        expect(typeof contract.methods.getData).to.equal('function');
+        expect(typeof contract.methods.setData).to.equal('function');
+    });
+    it("should reject if contract function is called without setting a provider", async function () {
+        const account = web3.eth.accounts.privateKeyToAccount(process.env.ETH_PRIVATE_KEY);
+        await expect(web3.eth.getBalance(account.address)).rejectedWith(/Provider not set or invalid/);
+    });
 });
